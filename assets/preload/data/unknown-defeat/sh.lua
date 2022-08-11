69056e3b6c392214730747f0100f49c2390068b1
VLkkkkkk <110204776+VLkkkkkk@users.noreply.github.com>
2022-08-11 10:33:04 -0300

    Add files via upload

---
+++ assets/preload/data/unknown-defeat/sh.lua
@@ -0,0 +1,13 @@
+shake = '0.05, 0.01'
+
+function onEvent(name,value1,value2)
+if name == 'Asbel' then
+if value1 == 'shake' then
+shake = value2
+end
+end
+end
+
+function opponentNoteHit(id, noteData, noteType, isSustainNote)
+triggerEvent('Screen Shake', shake, '0, 0');
+end
